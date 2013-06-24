95ae0ccb5391fff9e6b11994fd9a2094730dc3b0
Eduardo Costa <m4c0@users.noreply.github.com>
2013-06-24 16:35:02 -0300

    Acertando posição a partir do centro da view

--- SunnyCoffee/SCFCoffeeShopScene.m
+++ SunnyCoffee/SCFCoffeeShopScene.m
@@ -18,19 +18,18 @@
 - (id)init {
     if (self = [super initWithSize:CGSizeMake(10, 16)]) {
         self.scaleMode = SKSceneScaleModeFill;
+        self.anchorPoint = CGPointMake(0.5, 0.5);
         
         SKSpriteNode * backTile = [SCFFloorTileNode new];
 
         SCFTiledNode * back = [SCFTiledNode tileNodeWithNodeTile:backTile andDimensions:CGSizeMake(9, 5)];
-        back.position = CGPointMake(self.size.width / 2, self.size.height / 2);
         [self addChild:back];
 
         SKSpriteNode * girl = [SCFPlayerNode new];
-        girl.position = CGPointMake(self.size.width / 2, self.size.height / 2);
         [self addChild:girl];
         
         SCFInteractiveNode * chest = [SCFInteractiveNode new];
-        chest.position = CGPointMake(2, 7);
+        chest.position = CGPointMake(-3, -1);
         [self addChild:chest];
     }
     return self;
